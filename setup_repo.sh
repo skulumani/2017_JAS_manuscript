11bb7c61db8d24c9a0639be84e62cb2cbe49df31
Shankar Kulumani <shanks.k@gmail.com>
2017-01-16 20:03:12 -0500

    Add repo remote setup script

--- setup_repo.sh
+++ setup_repo.sh
@@ -3,15 +3,16 @@
 # add the correct remote repositories
 git_repo="git@github.com:skulumani/2017_JAS.git"
 bb_repo="git@bitbucket.org:shankarkulumani/2017_jas.git"
+fdcl_repo="git@github.com:fdcl-gwu/2017_JAS.git"
 
 printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
 printf "Github: $git_repo\n"
 printf "Bitbucket: $bb_repo\n\n"
 
 
-git remote add origin $git_repo
 git remote set-url origin --push --add $bb_repo
 git remote set-url origin --push --add $git_repo
+git remote add fdcl $fdcl_repo
 
 printf "Finished! The new remotes are listed.\n"
 printf "\n"
